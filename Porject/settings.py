748dc9729f351aadf0282652288846be37ae6b74
Abubaker <beeeeeeko36@gmail.com>
2022-02-09 00:07:21 +0200

    My kjkdf 222

--- Porject/settings.py
+++ Porject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-#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
+#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
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ixn&qz^a0x&^_lzw(esl2oq-#r8y+ef*nvid5&*l2id=(!3sps'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Porject/urls.py
+++ Porject/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
 
@@ -25,6 +27,11 @@ urlpatterns = [
     path('car/', include('browsr_cars.urls', namespace='cars')),
     path('contact_us/', include('contact.urls', namespace='contact_us')),
     path('abuot/', include('abuot_team.urls', namespace='abuot_team')),
+
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 
 
 
